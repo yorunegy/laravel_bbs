01ae582de45daaf084dc5784e7e064d708d69733
vagrant <vagrant@homestead>
2020-03-07 18:01:21 +0000

    Added edit comment

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Post;
+use App\Comment;
 
 class CommentsController extends Controller
 {
@@ -18,5 +18,30 @@ class CommentsController extends Controller
         $post->comments()->create($params);
 
         return redirect()->route('posts.show', ['post' => $post]);
+    }
+
+    public function edit($comment_id)
+    {
+        $comment = Comment::findOrFail($comment_id);
+
+        return view('comments.edit', [
+            'comment' => $comment,
+        ]);
+    }
+
+    public function update($comment_id, Request $request)
+    {
+        $params = $request->validate([
+            'body' => 'required|max:200',
+        ]);
+        $comment = Comment::findOrFail($comment_id);
+        $comment->fill($params)->save();
+
+        return redirect()->route('posts.show', ['post' => $comment->post_id]);
+    }
+
+    public function destroy($post_id)
+    {
+        # code...
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PostsController@index')->name('top');
 Route::resource('posts', 'PostsController')->only(['create', 'store', 'show', 'edit', 'update', 'destroy']);
-Route::resource('comments', 'CommentsController')->only(['store']);
+Route::resource('comments', 'CommentsController')->only(['store', 'edit', 'update', 'destroy']);
